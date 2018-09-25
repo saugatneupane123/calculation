b10fbaeffc6b48a2bf00b50ef2d2b8645a028bc4
SAUGAT NEUPANE <saugat.neupane25@gmail.com>
2018-09-25 09:53:13 +0545

    implemented and tested add function

---
+++ AirthmeticDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirthmeticDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 8;
+            int sum = add(a, b);
+            Console.WriteLine("sum:{0}",sum);
+        }
+
+        static int add(int a,int b)
+        {
+          return(a+b);  
+        }
+
+   
+    }
+}
